4bfa79ddeb87eb1241b0e9b28297ce0ebb3aee05
Nikola <nikola.r.totev@gmail.com>
2019-12-23 18:08:18 +0200

    Added class Activity

---
+++ C_Sharp_Course_Project/Core/Activity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    class Activity
+    {
+        public string Category { get; set; }
+        public string Description { get; set; }
+
+        public int UsingTemplate { get; set; }
+        public int CorrespondingRoom { get; set; }
+
+        public ActivityStatus CurActivityStatus { get; set; }
+        public DateTime ScheduleDateTime { get; set; }
+
+        //TODO Add methods for creating activity.
+    }
+}
---
+++ C_Sharp_Course_Project/Core/ActivityManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    enum ActivityStatus
+    {
+        Scheduled,
+        InProgress,
+        Finished
+    }
+
+    class ActivityManager
+    {
+       
+    }
+}
