8830e198f70731c619cb88d719bce95f98e2d184
Ivaylo-Valentinov <58509466+Ivaylo-Valentinov@users.noreply.github.com>
2019-12-30 14:41:15 +0200

    Add Room properties and constructor with parameters. Implement Singleton pattern for Room manager

--- C_Sharp_Course_Project/Core/RoomManager.cs
+++ C_Sharp_Course_Project/Core/RoomManager.cs
@@ -9,6 +9,22 @@ namespace Core
     public class RoomManager
     {
         ///TODO Implement singleton pattern (see program manager for example)
+
+        private static RoomManager m_Instance;
         public List<string> RoomTypes { get; set; }
+        
+        private RoomManager()
+        {
+            m_Instance = this;
+        }
+
+        public static RoomManager GetInstance()
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = new RoomManager();
+            }
+            return m_Instance;
+        }
     }
 }
--- C_Sharp_Course_Project/Core/SerializationManager.cs
+++ C_Sharp_Course_Project/Core/SerializationManager.cs
@@ -18,10 +18,37 @@ namespace Core
         static readonly string m_ProgramFolder = System.IO.Path.Combine(m_LocalAppDataPath, "MallManager_DATA");
         static readonly string m_ActivityManagerSave = System.IO.Path.Combine(m_ProgramFolder, "ActivityManager.json");
         //TODO create path for room serializationFile
+        static readonly string m_RoomManagerSave = System.IO.Path.Combine(m_ProgramFolder, "RoomManager.json");
 
-        public static void SerializeRoomManager() //TODO Add room parameter;
+        public static void SaveRoomManager(RoomManager managerToSave) //TODO Add room parameter;
         {
             //Create serialization code;
+            using (StreamWriter file = File.CreateText(m_RoomManagerSave))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, managerToSave);
+            }
+        }
+
+        /// <summary>
+        /// Gets last save of room manager. If no save is found a new one is created.
+        /// </summary>
+        /// <returns></returns>
+        public static RoomManager GetRoomManager()
+        {
+            if (!File.Exists(m_RoomManagerSave))
+            {
+                RoomManager managerToReturn = RoomManager.GetInstance();
+                ExceptionManager.OnFileNotFound(m_RoomManagerSave);
+                SaveRoomManager(managerToReturn);
+                return managerToReturn;
+            }
+
+            using (StreamReader file = File.OpenText(m_RoomManagerSave))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                return (RoomManager)serializer.Deserialize(file, typeof(RoomManager));
+            }
         }
 
         /// <summary>
@@ -46,7 +73,7 @@ namespace Core
 
             if (!File.Exists(m_ActivityManagerSave))
             {
-                ActivityManager managerToReturn = new ActivityManager(ProgramManager.GetInstance());
+                ActivityManager managerToReturn = ActivityManager.GetInstance();
                 ExceptionManager.OnFileNotFound(m_ActivityManagerSave);
                 SaveActivityManager(managerToReturn);
                 return managerToReturn;
