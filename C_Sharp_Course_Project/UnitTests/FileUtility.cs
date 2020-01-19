d97495d43a1dfbb9e7486750b341563b8edde699
Nikola <nikola.r.totev@gmail.com>
2020-01-19 20:19:16 +0200

    Add FileUtility in Unit Test project.

---
+++ C_Sharp_Course_Project/UnitTests/FileUtility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using NUnit.Framework.Internal;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class FileUtility
+    {
+        [Test]
+        public void CreateActivityConfigFile()
+        {
+            string savePath = SerializationManager.ActivityConfigSave;
+            ActivityConfig newConfig = new ActivityConfig();
+            List<string> newCategories = new List<string>(){"Light Maintenance","Heavy Maintenance", "Cleaning", "Inspection"};
+            List<Activity> newTemplates = new List<Activity>();
+            
+            Activity newTemplate = new Activity(isTemplate:true, category:"Cleaning", status:ActivityStatus.Scheduled,scheduleDate:new DateTime(2020,12,12) );
+
+            newTemplates.Add(newTemplate);
+            newConfig.Categories = newCategories;
+            newConfig.Templates = newTemplates;
+            SerializationManager.SaveActivityConfigFile(newConfig);
+        }
+
+    }
+}
