c80fbfa3dd3bed7d14c47612aaf1aa1daa3245a2
Nikola <nikola.r.totev@gmail.com>
2020-01-22 00:35:41 +0200

    Created MallManager

---
+++ C_Sharp_Course_Project/Core/MallManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Input;
+using System.Windows.Forms;
+
+namespace Core
+{
+    public class MallManager
+    {
+        
+    }
+}
